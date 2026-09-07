6549a2d47e4da1621b6fde03d4f7c90be66c0af3
Eashan-H <eashan.hasija@gmail.com>
2026-09-07 14:38:39 +0530

    Solve - A. Watermelon

---
+++ Codeforces/A. Watermelon/README.md
@@ -0,0 +1,9 @@
+# A. Watermelon
+
+**Problem:** [A. Watermelon](https://codeforces.com/problemset/problem/4/A)
+
+**Rating:** 800
+
+**Tags:** `brute force`, `math`
+
+**Language:** java 21,java 17,java 11,java
---
+++ Codeforces/A. Watermelon/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = 3 - 2; // Testing
+
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            if (n % 2 == 0 && n > 2) {
+                System.out.println("YeS");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
